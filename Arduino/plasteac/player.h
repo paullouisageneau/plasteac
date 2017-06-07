a3d52ca6b99e278ab25baa20ff581b249b38fb31
Paul-Louis Ageneau <paul-louis@ageneau.org>
2017-06-07 15:22:34 +0200

    Fixed tabs

--- Arduino/plasteac/player.h
+++ Arduino/plasteac/player.h
@@ -27,104 +27,104 @@ public:
   }
 
   // Start playing melody
-	void play(const char *melody, bool looped = false)
-	{
-		current = melody;
-		if(looped) next = melody;
-		else next = NULL;
-		left = 0;
-	}
+  void play(const char *melody, bool looped = false)
+  {
+    current = melody;
+    if(looped) next = melody;
+    else next = NULL;
+    left = 0;
+  }
 
   // Stop melody
-	void stop(void)
-	{
-		current = NULL;
-		left = 0;
-	}
+  void stop(void)
+  {
+    current = NULL;
+    left = 0;
+  }
 
-	// Update melody, must be called on each quarter of a beat
-	void sync(long bpm)
-	{
-		// Check if no melody is set
-		if(!current) return;
+  // Update melody, must be called on each quarter of a beat
+  void sync(long bpm)
+  {
+    // Check if no melody is set
+    if(!current) return;
 
-		// Check if a note is playing
-		if(left)
-		{
-			left--;
-			if(left) return;
-		}
+    // Check if a note is playing
+    if(left)
+    {
+      left--;
+      if(left) return;
+    }
 
-		// Check for end of the melody string
-		if(!current[0] || !current[1] || !current[2])
-		{
-			current = next;
-			if(!current) return;
-		}
+    // Check for end of the melody string
+    if(!current[0] || !current[1] || !current[2])
+    {
+      current = next;
+      if(!current) return;
+    }
 
-		// Read note in the melody string
-		unsigned char d = (unsigned char)current[0];
-		unsigned int duration = (d >= 0x41 ? d - 0x41 + 10 : d - 0x30);
-		unsigned int octave = (unsigned char)current[2] - 0x30;
-		char note = current[1];
+    // Read note in the melody string
+    unsigned char d = (unsigned char)current[0];
+    unsigned int duration = (d >= 0x41 ? d - 0x41 + 10 : d - 0x30);
+    unsigned int octave = (unsigned char)current[2] - 0x30;
+    char note = current[1];
 
-		// 0 is interpreted as a whole note, or semibreve
-		if(duration == 0) duration = 16;
+    // 0 is interpreted as a whole note, or semibreve
+    if(duration == 0) duration = 16;
 
-		// Update status
-		left = duration;
-		current+= 3;
+    // Update status
+    left = duration;
+    current+= 3;
 
-		// Play note
-		unsigned int frequency = pitch(octave, note);
-		if(frequency) 
-		{
-			unsigned long unit = 60000L/(bpm*4);
-			noTone(speakerPin);
-			tone(speakerPin, frequency, unit*duration - unit/2);
-		}
-	}
+    // Play note
+    unsigned int frequency = pitch(octave, note);
+    if(frequency) 
+    {
+      unsigned long unit = 60000L/(bpm*4);
+      noTone(speakerPin);
+      tone(speakerPin, frequency, unit*duration - unit/2);
+    }
+  }
 
-private:	
-	const char *current;
-	const char *next;
-	unsigned int left;  // sync calls left for current note
-	
-	// Return frequency given octave number and note name
-	static unsigned int pitch(unsigned int octave, char note)
-	{
-		// Note names array
-		static const unsigned int countNotes = 12;
-		static const char Notes[] = { 'C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B' };
+private:  
+  const char *current;
+  const char *next;
+  unsigned int left;  // sync calls left for current note
+  
+  // Return frequency given octave number and note name
+  static unsigned int pitch(unsigned int octave, char note)
+  {
+    // Note names array
+    static const unsigned int countNotes = 12;
+    static const char Notes[] = { 'C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B' };
 
-		// Frequencies array
-		static const unsigned int countFreqs = countNotes*7 + 4;
-		static const unsigned int Freqs[] = {   33,   35,   37,   39,   41,   44,   46,   49,   52,   55,   58,   62,
-							65,   69,   73,   78,   82,   87,   93,   98,  104,  110,  117,  123,
-							131,  139,  147,  156,  165,  175,  185,  196,  208,  220,  233,  247,
-							262,  277,  294,  311,  330,  349,  370,  392,  415,  440,  466,  494,
-							523,  554,  587,  622,  659,  698,  740,  784,  831,  880,  932,  988,
-							1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976,
-							2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951,
-							4186, 4435, 4699, 4978 };
+    // Frequencies array
+    static const unsigned int countFreqs = countNotes*7 + 4;
+    static const unsigned int Freqs[] = {   33,   35,   37,   39,   41,   44,   46,   49,   52,   55,   58,   62,
+              65,   69,   73,   78,   82,   87,   93,   98,  104,  110,  117,  123,
+              131,  139,  147,  156,  165,  175,  185,  196,  208,  220,  233,  247,
+              262,  277,  294,  311,  330,  349,  370,  392,  415,  440,  466,  494,
+              523,  554,  587,  622,  659,  698,  740,  784,  831,  880,  932,  988,
+              1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976,
+              2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951,
+              4186, 4435, 4699, 4978 };
 
-		// Rest
-		if(note == 'R' || note == 'r')
-			return 0;
+    // Rest
+    if(note == 'R' || note == 'r')
+      return 0;
 
-		// Find note index
-		unsigned int n = 0;
-		while(Notes[n] != note)
-		if(++n == countNotes)
-			return 0;
+    // Find note index
+    unsigned int n = 0;
+    while(Notes[n] != note)
+    if(++n == countNotes)
+      return 0;
 
-		// Frenquency index
-		if(octave == 0) return 0;
-		unsigned int p = countNotes*(octave-1) + n;
-		
-		if(p < countFreqs) return Freqs[p];
-		else return 0;
-	}
+    // Frenquency index
+    if(octave == 0) return 0;
+    unsigned int p = countNotes*(octave-1) + n;
+    
+    if(p < countFreqs) return Freqs[p];
+    else return 0;
+  }
 };
 
 
